8842172a4689a97ce96d9c7413a85f244182b682
Vaishanavi Prasad <vaishanaviprasad1118@gmail.com>
2020-07-09 23:02:46 +0530

    uploading

---
+++ src/SearchBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBox = ({searchfield,SearchChange})=>{
+	return(
+		<div>
+			<input 
+			className='pa3 ba b--green bg-lightest-blue'
+			type='search' 
+			placeholder='search robots'
+			onChange= {SearchChange}
+
+			/>
+		</div>
+
+
+		);
+
+
+
+
+}
+export default SearchBox;
